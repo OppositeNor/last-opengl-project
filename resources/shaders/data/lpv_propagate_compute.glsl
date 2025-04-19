f893adbe76d1ab92798ccc9e232855576d84bcb4
OppositeNor <oppositenormal@outlook.com>
2025-04-19 12:51:23 -0400

    fix linux build errors

--- resources/shaders/data/lpv_propagate_compute.glsl
+++ resources/shaders/data/lpv_propagate_compute.glsl
@@ -2,8 +2,15 @@
 
 layout(local_size_x = 4, local_size_y = 4, local_size_z = 4) in;
 
-uniform layout(binding=0,rgba32f) image3D lpv_voxels;
+uniform layout(binding=0,rgba32f) image3D lpv_voxels_r;
+uniform layout(binding=0,rgba32f) image3D lpv_voxels_g;
+uniform layout(binding=0,rgba32f) image3D lpv_voxels_b;
+
+void propagate_lpv(vec3 position, vec3 direction, vec3 color) {
+    
+}
 
 void main() {
-    // TODO
+
 }
+
